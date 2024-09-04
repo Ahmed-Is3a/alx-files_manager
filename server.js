ed7b4bf5fec1bf2c5f1a5032948f7e95c1bb55f4
ahmed <ahmedissaten@gmail.com>
2024-09-04 09:39:19 +0200

    using env_loader ;

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import injectRoutes from './routes';
+import envLoader from './utils/env_loader';
 
 const server = express();
-
 server.use(express.json({ limit: '200mb' }));
 
 injectRoutes(server);
 
+envLoader();
 const port = process.env.PORT || 5000;
 const env = process.env.npm_lifecycle_event || 'dev';
 server.listen(port, () => {
