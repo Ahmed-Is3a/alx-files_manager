84e0c18da01450ea469124b34a73ec75df495266
ahmed <ahmedissaten@gmail.com>
2024-09-04 09:05:16 +0200

    use function for routes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const routes = require('./routes/index');
+import express from 'express';
+import injectRoutes from './routes';
+
+const server = express();
+
+server.use(express.json({ limit: '200mb' }));
+
+injectRoutes(server);
 
 const port = process.env.PORT || 5000;
-const app = express();
-
-app.use('/', routes);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const env = process.env.npm_lifecycle_event || 'dev';
+server.listen(port, () => {
+  console.log(`[${env}] API has started listening at port:${port}`);
 });
+
+export default server;
